52ac21fff5ad8dde70ca4e9b8c92b02b66349088
AtlantaPatterson <atlantarpatterson@gmail.com>
2022-03-26 12:19:09 -0700

    commit tree ac28a051771075aaa7ef244627e336158a178b36

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sammy"]
+function findMatching(drivers, string) {
+    let answer = drivers.filter(element => element.toLowerCase() == string.toLowerCase())
+    return answer
+    }
+
+function fuzzyMatch(drivers, string) {
+    let letterMatch = drivers.filter(element => element[0] == string[0])
+    return letterMatch
+}
+
+function matchName(driver, string) {
+    let nameMatching = driver.filter(driver => driver.name ===string)
+    return nameMatching
+}
